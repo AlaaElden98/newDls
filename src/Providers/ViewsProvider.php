61cf5aff3585d9de8dc62bff82c034bb7f5f1f51
Alaa Abdelfattah <a.abdelfattah@yodawy.com>
2022-08-25 17:17:09 +0200

    Revert

--- src/Providers/ViewsProvider.php
+++ src/Providers/ViewsProvider.php
@@ -9,9 +9,5 @@ class ViewsProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__ . '/../views', 'dls');
-        $this->publishes([
-            __DIR__ . '/../public/css/app.css' => public_path('vendor/css/app.css'),
-            __DIR__ . '/../public/css/theme.css' => public_path('vendor/css/theme.css'),
-        ], 'public');
     }
 }
