17cac8e680e9b9b35085f66ea6062bc9b1659750
Alaa Abdelfattah <a.abdelfattah@yodawy.com>
2022-08-29 15:43:53 +0200

    Publish public files

--- src/Providers/ViewsProvider.php
+++ src/Providers/ViewsProvider.php
@@ -9,6 +9,9 @@ class ViewsProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
+            __DIR__ . '/../public' => public_path(''),
+        ], 'public');
+        $this->publishes([
             __DIR__ . '/../views' => resource_path('views'),
         ], 'views');
         $this->loadViewsFrom(__DIR__ . '/../views', 'dls');
